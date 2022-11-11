bfc75192645794bee2cc55e11ec30a0d0b440d14
Sérgio Teixeira <sergioramosteixeira@gmail.com>
2022-11-11 19:49:35 +0000

    add menu to ex2

--- tp1_ex2/bin/tp1_ex2.dart
+++ tp1_ex2/bin/tp1_ex2.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:tp1_ex2/tp1_ex2.dart' as tp1_ex2;
 import 'dart:math';
 
 void main(List<String> arguments) {
   var random = Random();
   var aleatorio = 90 + random.nextInt(150 - 90);
-  var num = 0;
+  int num, opcao = 0;
   List<int> numeros = [];
   List<int> impares = [];
   for(var i=0; i < aleatorio; i++){
@@ -13,15 +15,50 @@ void main(List<String> arguments) {
     (num%2!=0) ? impares.add(num) : '';
   }
   impares.sort((b, a) => a.compareTo(b));
+  while(opcao!=9){
+    print("""
+    ------------------ :: MENU :: ------------------
+    
+    Selecione a opção pretendida:
+
+    1 - Mostrar maior e menor elemento do conjunto
+    2 - Mostrar amplitude do conjunto
+    3 - Listar números ímpares em ordem decrescente
+    4 - Mostrar conjunto completo
+
+    9 - Sair
+
+    Opção : """);
+
+    opcao = int.parse(stdin.readLineSync()!);
+    
+    if(opcao==1){
+      print("O maior numero é ${maior(numeros)} e o menor numero é ${menor(numeros)}");
+    } 
+    if(opcao==2){
+      print("A amplitude do array é ${amplitude(numeros)}");
+    }
+    if(opcao==3){
+      print("Ímpares ordenados:");
+      print(imprime(impares));
+    }
+    if(opcao==4){
+      print(imprime(numeros));
+    }
+    if(![1,2,3,4].contains(opcao)){
+      print("Opção Inválida");
+    }
+    print("""
+          Clique <Enter> para continuar...""");
+    stdin.readLineSync();
+  }
   
-  print("O maior numero é ${maior(numeros)} e o menor numero é ${menor(numeros)}");
 
-  print("A amplitude do array é ${amplitude(numeros)}");
+  
 
-  print(imprime(numeros));
+  
 
-  print("Ímpares ordenados:");
-  print(imprime(impares));
+  
 
 }
 
